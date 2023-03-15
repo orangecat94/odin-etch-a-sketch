acc4afac3b41976de771fb09369e538daf242b80
orangecat <shaunchua.edu@gmail.com>
2023-03-16 00:14:32 +0800

    add createBoard function to dynamically create custom grid

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-const board = document.querySelector('div.board');
-board.style.gridTemplate = "repeat(16, 1fr) / repeat(16, 1fr)";
+function createBoard(size) {
+    const board = document.querySelector('div.board');
+
+    board.style.gridTemplate = `repeat(${size}, 1fr) / repeat(${size}, 1fr)`;
+
+    // create divs
+    const numDivs = size * size;
+
+    for (let i = 0; i < numDivs; i++) {
+        const div = document.createElement('div');
+        div.style.backgroundColor = 'yellow';
+        div.style.borderColor = 'black';
+        div.style.borderStyle = 'solid';
+        div.style.borderWidth = 'thin';
+        board.appendChild(div);
+    }
+}
+
+createBoard(20);
